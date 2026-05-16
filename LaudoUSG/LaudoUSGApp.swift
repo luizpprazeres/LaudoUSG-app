dbea38deaed244f95406a67b1cc9b2f4bbec57c3
Luiz Paulo de Souza Prazeres <contato@luizprazeres.com.br>
2026-05-16 19:21:03 -0300

    Initial Commit

---
+++ LaudoUSG/LaudoUSGApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaudoUSGApp.swift
+//  LaudoUSG
+//
+//  Created by Luiz Prazeres on 16/05/26.
+//
+
+import SwiftUI
+
+@main
+struct LaudoUSGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
